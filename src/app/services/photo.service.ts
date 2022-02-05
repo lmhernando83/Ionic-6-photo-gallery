4dbec61b80faeb33361a8d10b008b3b3be5831ba
Luis Hernando <lmhernando83@gmail.com>
2022-02-05 19:09:13 +0100

    feat: displaying the images

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,22 +1,38 @@
 import { Injectable } from '@angular/core';
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 
+export interface IUserPhoto {
+  filepath: string;
+  webviewPath: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 
 export class PhotoService {
 
+  public photos: IUserPhoto[] = []
+
   constructor(){}
 
-  public async addNewToGallery():Promise<Photo>{
+  public async addNewToGallery():Promise<void>{
     // Take a photo
-    return await Camera.getPhoto({
+    const capturedPhoto:Promise<Photo> = await Camera.getPhoto({
+      quality: 100,
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
-      quality: 100
     })
     .catch(Error => Error) //<= Avoids display errors on browser console
+
+    console.log('Foto ==>', capturedPhoto)
+
+    this.photos.unshift({
+      filepath: 'Soon...',
+      webviewPath: (await capturedPhoto).webPath
+    })
+
+    console.log('Array de fotos',this.photos)
   }
 
 }
